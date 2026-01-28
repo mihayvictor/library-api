ef7dedd139d046a83eaa4eb4f45b576383b12c9d
mihayvictor <m.victorcezario@gmail.com>
2026-01-28 20:41:06 -0300

    Project created

---
+++ src/test/java/io/github/mihayvictor/library_api/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mihayvictor.library_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
